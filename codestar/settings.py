e127b86b3d7fb9363dad408acdab34aafe019ef3
Craig Dickerson <craigdickerson@hotmail.com>
2024-09-04 09:03:52 +0000

    Create about app

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-craigdickers-djangoblog-bvhx071ww0p.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'django_summernote',
+    'about',
 ]
 
 MIDDLEWARE = [
